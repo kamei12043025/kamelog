dd48f0fb7289803e398a69f27c5b6623f2fc3455
101design <101design@101designnoMacBook-Pro.local>
2024-10-04 14:21:42 +0900

    Newtを追加

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
+
+//src/env.d.ts で ImportMetaEnv を以下のように設定することで、環境変数の自動補完が効くようになります。
+interface ImportMetaEnv {
+    readonly NEWT_SPACE_UID: string
+    readonly NEWT_CDN_API_TOKEN: string
+}
