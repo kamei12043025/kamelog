d4bfffc167e57d74d0bfa839f31bb91aeea19dcb
101design <101design@101designnoMacBook-Pro.local>
2024-10-07 10:35:16 +0900

    ブログのインデックスページを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,25 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [tailwind()],
+
+  vite: {
+    resolve: {
+      alias: {
+        '@components': '/src/components',
+      },
+    },
+  },
+  // 画像の最適化のためHTTPSホストからのリモート画像のみ許可
+  image: {
+    remotePatterns: [{ protocol: 'https' }],
+  },
+  server: {
+    host: true,
+    port: 4321,
+  },
+})
