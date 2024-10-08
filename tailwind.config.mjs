eab4a598758f9f1d7c68fc0dd381b9c6f2c27a1b
101design <101design@101designnoMacBook-Pro.local>
2024-10-08 17:30:34 +0900

    フッターを追加

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -19,19 +19,46 @@ export default {
   plugins: [
     function ({ addComponents }) {
       addComponents({
+        //テキストスタイル
         '.base-text-2xl': {
           '@apply text-2xl md:text-3xl font-bold theme-color tracking-wide': {},
         },
+        
+        //テーマカラー
         '.theme-color': {
-          '@apply text-base-color hover:text-text-hover dark:text-white dark:hover:text-gray-300':
+          '@apply text-black dark:text-white':
             {},
         },
+        
+        '.theme-color-other': {
+          '@apply text-white dark:text-black':
+            {},
+        },
+
+        '.theme-bg-color': {
+          '@apply bg-black dark:bg-white':
+            {},
+        },
+
+        '.theme-group-hover-border': {
+          '@apply bg-purple-800 dark:bg-custom-purple':
+            {},
+        },
+
+        '.theme-group-hover-text': {
+          '@apply group-hover:text-purple-800 dark:group-hover:text-custom-purple':
+            {},
+        },
+
+        //ナビゲーション
         '.nav-link-text': {
           '@apply theme-color uppercase text-base': {},
         },
         '.nav-link-current': {
-          '@apply uppercase text-base md:text-xl': {},
+          '@apply uppercase text-base': {},
         },
+
+        //ダイアログメニュー
         '.dialog-menu': {
           '@apply py-4 px-4 flex justify-between items-center bg-gray-100 dark:bg-custom-dark border-b border-gray-300 dark:border-gray-600 hover:bg-gray-200 dark:hover:bg-gray-600':
             {},
@@ -39,6 +66,19 @@ export default {
         '.dialog-arrow-animation': {
           '@apply transition-transform duration-300 ease-in-out transform group-hover:translate-x-1 theme-color':
             {},
+        },
+        //tabボタン
+        '.tab-button': {
+          '@apply py-2 w-full focus:outline-none': {},
+        },
+        '.tab-button.selected': {
+          '@apply bg-purple-800 dark:bg-purple-900 text-white': {},
+        },
+        '.tab-button.unselected': {
+          '@apply bg-white dark:bg-black text-black dark:text-white': {},
+        },
+        '.tab-button:first-child': {
+          '@apply border-r border-black dark:border-white': {},
         },
       })
     },
